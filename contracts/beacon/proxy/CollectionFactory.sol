6f316b77b5caf244a8eb3e3024f334d9e909fa77
Alin Mihai Barbatei <alin.barbatei@gmail.com>
2023-04-25 17:19:33 +0300

    extra fixes

--- contracts/beacon/proxy/CollectionFactory.sol
+++ contracts/beacon/proxy/CollectionFactory.sol
@@ -4,10 +4,9 @@ pragma solidity ^0.8.15;
 import { Ownable2Step } from "openzeppelin/access/Ownable2Step.sol";
 import { Pausable } from "openzeppelin/security/Pausable.sol";
 import { UpgradeableBeacon } from "openzeppelin/proxy/beacon/UpgradeableBeacon.sol";
-
 import { Address } from "openzeppelin/utils/Address.sol";
 import { CollectionProxy } from "./CollectionProxy.sol";
-
+import { IERC5131 } from "../interfaces/IERC5131.sol";
 
 contract CollectionFactory is Ownable2Step, Pausable {
 
@@ -16,8 +15,15 @@ contract CollectionFactory is Ownable2Step, Pausable {
     //////////////////////////////////////////////////////////////*/
 
     /**
+     * @notice Event emitted when a beacon was marked as followed by the factory
+     * @dev emitted when deployBeacon or addBeacon is called
+     * @param beacon the marked beacon address
+     */
+    event BeaconAdded(address beacon);
+
+    /**
      * @notice Event emitted when a collection (proxy) was deployed
-     * @dev emitted when deployDefaultCollection or deployCollection is called
+     * @dev emitted when deployCollection is called
      * @param beaconAddress the used beacon address for the collection
      * @param collectionProxy the new collection proxy address
      */
@@ -27,7 +33,7 @@ contract CollectionFactory is Ownable2Step, Pausable {
      * @notice Event emitted when a collection (proxy) was updated (had it's implementation change)
      * @dev emitted when updateCollection is called
      * @param proxyAddress the proxy address whose beacon has changed
-     * @param beacon the new beacon that was used
+     * @param beacon the new beacon address that is used
      */
     event CollectionUpdated(address proxyAddress, address beacon);
 
@@ -39,7 +45,7 @@ contract CollectionFactory is Ownable2Step, Pausable {
     address[] public beacons;
 
     mapping(address => bool) public beaconState;
-    mapping(address => bool) public collectionState;
+    mapping(address => address) public collectionToBeacon;
 
     /*//////////////////////////////////////////////////////////////
                            Constructor / Initializers
@@ -62,15 +68,12 @@ contract CollectionFactory is Ownable2Step, Pausable {
         returns (address beacon) 
     {
             beacon = address(new UpgradeableBeacon(implementation));
-            beacons.push(beacon);
-            beaconState[beacon] = true;
+            _saveBeacon(beacon);
     }
 
     function addBeacon(address beacon) external onlyOwner {
         require(Address.isContract(beacon), "CollectionFactory: beacon is not a contract");
-        beacons.push(beacon);
-        beaconState[beacon] = true;
-    }
+        _saveBeacon(beacon);    }
 
     function deployCollection(address beacon, bytes calldata initializationArgs) 
         public 
@@ -80,8 +83,9 @@ contract CollectionFactory is Ownable2Step, Pausable {
     {   
         CollectionProxy collectionProxy = new CollectionProxy(beacon, initializationArgs);
         collection = address(collectionProxy);
+
         collections.push(collection);
-        collectionState[collection] = true;
+        collectionToBeacon[collection] = beacon;
 
         emit CollectionDeployed(beacon, collection);
     }
@@ -93,11 +97,13 @@ contract CollectionFactory is Ownable2Step, Pausable {
      */
     function updateCollection(address collection, address beacon, bytes memory updateArgs) 
         external 
-        onlyOwner
+        onlyCollectionOwner
         collectionExists(collection)
         beaconIsAvailable(beacon)
     {
         CollectionProxy(payable(collection)).changeBeacon(beacon, updateArgs); 
+        collectionToBeacon[collection] = beacon;
+        
         emit CollectionUpdated(collection, beacon);
     }
 
@@ -112,10 +118,10 @@ contract CollectionFactory is Ownable2Step, Pausable {
         UpgradeableBeacon(beacon).upgradeTo(implementation);
     }
 
-    /*//////////////////////////////////////////////////////////////
-                           View functions
-    //////////////////////////////////////////////////////////////*/
-
+    /** 
+     * @notice helper function that retrieves all implementations tracked by the factory
+     * @return list of implementation addresses used by the proxy
+     */
     function getImplementations() external view returns (address[] memory) {
         uint256 beaconCount = beacons.length;
         address [] memory implementations = new address[](beaconCount);
@@ -126,13 +132,54 @@ contract CollectionFactory is Ownable2Step, Pausable {
         return implementations;
     }
 
+    /** 
+     * @notice helper function that retrieves all implementations tracked by the factory
+     * @return list of implementation addresses used by the proxy
+     */
+    function beaconOf(address collection) external view collectionExists(collection) returns (address) {
+        return CollectionProxy(payable(collection)).beacon();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                Internal, private and modifier functions
+    //////////////////////////////////////////////////////////////*/
+
+    /** 
+     * @notice saves the beacon address into internal tracking
+     * @dev beacon address sanity checks must be done before calling this function
+     * @custom:event BeaconAdded
+     * @param beacon the beacon address to me marked
+     */
+    function _saveBeacon(address beacon) internal {
+        beacons.push(beacon);
+        beaconState[beacon] = true;
+        emit BeaconAdded(beacon);
+    }
+
+    /** 
+     * @notice Modifier used to check if a beacon is actually tracked by factory
+     * @param beacon the beacon address to check
+     */
     modifier beaconIsAvailable(address beacon) {
         require(beaconState[beacon], "CollectionFactory: beacon is not tracked");
         _;
     }
 
+    /** 
+     * @notice Modifier used to check if a collection is actually tracked by factory
+     * @param collection the collection address to check
+     */
     modifier collectionExists(address collection) {
-        require(collectionState[collection], "CollectionFactory: collection is not tracked");
+        require(collectionToBeacon[collection] != address(0x0), "CollectionFactory: collection is not tracked");
+        _;
+    }
+
+    /** 
+     * @notice Modifier used to check if a collection is actually tracked by factory
+     * @param collection the collection address to check
+     */
+    modifier onlyCollectionOwner(address collection) {
+        require(IERC5131(collection).owner() == msg.sender, "CollectionFactory: caller must be collection owner");
         _;
     }
 }
